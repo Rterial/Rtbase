576f0bd5036b4be3bfc597e1821906cd35f022d4
Carl Boneri <carl.boneri@fas-advisory.com>
2016-03-22 14:56:32 -0400

    Create rt_build.R
    Setup for new packages and the needed files and structure.

---
+++ R/rt_build.R
@@ -0,0 +1,98 @@
+#' Universal constants for package development
+#'
+#' \code{rt.check_scaffold}
+#' This step sets up a consistent directory structure
+#'
+rt.scaffold <- function(){
+
+  dirs <-
+    list(
+      inst = list(
+        www = list('style','js','images','templates'),
+        shiny = list('templates','ace','frangments'),
+        examples = list(''),
+        data = list('style_tables','index_tables')),
+      revdep = list(
+        tests = list('shiny_tests','R test','scraps')
+      )
+    )
+
+  dirs.structure <- llply(names(dirs),function(i)
+    sapply(names(dirs[[i]]),function(x)
+      c(i,paste0(i,"/",x),
+        paste0(i,"/",x,"/",
+               dirs[[i]][[x]]))
+    )
+  ) %>%
+    unlist %>%
+    as.character %>%
+    unique
+
+  sapply(dirs.structure,function(x)
+    suppressWarnings(dir.create(x))
+  )
+
+  dir.create('inst/www/templates/todo')
+  dir.create('inst/www/templates/datatable')
+  invisible()
+
+}
+
+
+
+#' Set up material design resources in the new directory
+#'
+#'
+#' \code{rt.setup}
+#'
+#'This will import the necessary files from alternate resources into the new
+#' directory.
+#'
+#'
+rt.setup <- function(){
+
+  if(!file.exists('inst')){
+    rt.scaffold()
+  }
+
+  rt_res <- list('mtrl_icons_svg.rds','mtrl_icons.rds','mtrl_colors.rds')
+  rds_base <-
+    lapply(rt_res,function(i)
+      curl::curl_download(
+        sprintf("https://github.com/CarlBoneri/RtAce/blob/master/inst/data/%s?raw=true",i),
+        sprintf('inst/data/style_tables/%s',i)
+    ))
+
+  r_rt_res <- list("MTRL_TAGS.R","MTRL_TABLE.R","MTRL_COLORS.R")
+
+  rt_html_r <-
+    lapply(r_rt_res,function(i)
+           readLines(
+             sprintf('https://raw.githubusercontent.com/Rterial/Rtbase/master/R/%s',i)
+           ) %>% paste0(collapse = "\n") %>% HTML %>% write(paste0('R/',i))
+    )
+
+
+
+  cp.file_types <- list(".html",".js",".css")
+  rt_todo <-
+    suppressWarnings(
+      lapply(cp.file_types,function(i)
+        sprintf("http://codepen.io/CarlBoneri/pen/mVMbEY%s",i) %>%
+          readLines %>% paste0(collapse = "\n") %>% HTML %>%
+          write(paste0("inst/www/templates/todo/todo_app",i))
+      )
+    )
+  rt_todo <-
+    suppressWarnings(
+      lapply(cp.file_types,function(i)
+        sprintf("http://codepen.io/CarlBoneri/pen/zqZYqR%s",i) %>%
+          readLines %>% paste0(collapse = "\n") %>% HTML %>%
+          write(paste0("inst/www/templates/datatable/mtrl_dt",i))
+      )
+    )
+      invisible()
+
+
+
+}
